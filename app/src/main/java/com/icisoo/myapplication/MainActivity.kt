1167756530ccf95d323283245de474c11a6ac004
xuexuan <384324069@qq.com>
2020-02-24 18:30:07 +0800

    测试get git log 换行

--- app/src/main/java/com/icisoo/myapplication/MainActivity.kt
+++ app/src/main/java/com/icisoo/myapplication/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //测试test yml
+        //测试test yml
     }
 }
